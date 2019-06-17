c2f69796ab5984ec8215ea640cddaa2cc7f30eb3
soanni <andrey.solodov@aurea.com>
2019-06-17 18:11:43 +0400

    consignment service, client added

---
+++ consignment-service/go.mod
@@ -0,0 +1,5 @@
+module github.com/soanni/go-microservices
+
+go 1.12
+
+require google.golang.org/grpc v1.21.1
---
+++ consignment-service/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"sync"
+
+	pb "github.com/soanni/go-microservices/consignment-service/proto/consignment"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+type Repository struct {
+	mu sync.RWMutex
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	repo.mu.Unlock()
+	return consignment, nil
+}
+
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+type service struct {
+	repo repository
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
+}
+
+func main() {
+	repo := &Repository{}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterShippingServiceServer(s, &service{repo})
+
+	reflection.Register(s)
+
+	log.Println("Running on port:", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
